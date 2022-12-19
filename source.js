a3d8b72b0549a374d8e33bf91586e52bc6f30792
O'Hoon <ohoon.kw@gmail.com>
2022-12-18 16:59:59 -0800

    fixed and ready

--- source.js
+++ source.js
@@ -1,13 +1,14 @@
 const file_system = require('fs');
 const archiver = require('archiver');
 const prompt = require('prompt-sync')();
-const zipCurrDir = (dir, name, vol, run = false) => {
-    console.log(`Starting zipping process for directory: ${dir}${name}\\${vol}`);
+const zipCurrDir = (dir, vol, run = false) => {
+    console.log(`Starting zipping process for directory: ${dir}${vol}`);
+    console.log(`run: ${run}`)
     if (! run) {
         return;
     }
-    const outputFileDirName = dir + name + '\\' + vol;
-    const sourceFileDirName = dir + name + '\\' + vol;
+    const outputFileDirName = dir + vol;
+    const sourceFileDirName = dir + vol;
     const output = file_system.createWriteStream(outputFileDirName + '.zip');
     const archive = archiver('zip');
     
@@ -32,26 +33,25 @@ const zipCurrDir = (dir, name, vol, run = false) => {
 
 const exceptions = []
 
-const dirCrawler = (baseDir, dirAppend = '', level = 0, run = false) => {
-    const readingDir = `${baseDir}\\${dirAppend}`;
+const dirCrawler = (baseDir, level = 0, run = false) => {
+    const readingDir = baseDir;
     const dirents = file_system.readdirSync(readingDir, {withFileTypes: true});
     dirents.forEach(dirent => {
         const res = `${readingDir}${dirent.name}`;
         if (level == 0) {
-            dirCrawler(baseDir, dirent.name, 1);
-        } else if (dirAppend) {
+            dirCrawler(baseDir + dirent.name + '\\', 1, run);
+        } else {
             var volName = dirent.name;
             // console.log(`baseDir: ${baseDir} - name: ${dirAppend} - fileName: ${volName}`);
             const volNumBeg = volName.indexOf('第');
             const volNumEnd = volName.indexOf('巻');
             if (volNumBeg >= 0 & volNumEnd >= 0  & ((volNumEnd - volNumBeg) == 3)) {
                 volName = `第${volName.substring(volNumBeg + 1, volNumEnd)}巻`;
-                // console.log(`Zipping ${baseDir}\\${dirAppend}\\${volName} into  ${baseDir}\\${dirAppend}\\第${volParam}巻`);
             } else {
-                // console.log(`Exception found for ${baseDir}\\${dirAppend}\\${volName}`);
-                exceptions.push(`${baseDir}\\${dirAppend}\\${volName}`);
+                // console.log(`Exception found for ${baseDir}${volName}`);
+                exceptions.push(`${baseDir}${volName}`);
             }
-            zipCurrDir(baseDir, dirAppend, volName, run);
+            zipCurrDir(baseDir, volName, run);
         }
     });
     if (level == 0) {
@@ -69,16 +69,15 @@ const processParams = () => {
     const funcName = process.argv[2];
     switch(funcName) {
         case 'zipCurrDir':
-            const dir = 'C:\\Users\\zacho\\OneDrive\\Documents\\MangaCopy\\';
-            const name = 'EX-ARM エクスアーム';
+            const dir = 'C:\\Users\\zacho\\OneDrive\\Documents\\MangaCopy\\EX-ARM エクスアーム\\';
             const vol = '第03巻';
-            console.log(`Processing coded values of dir: ${dir}${name}\\${vol}`);
+            console.log(`Processing coded values of dir: ${dir}${vol}`);
             var run = false;
-            if (process.argv.length == 5 & process.argv[4] == 'run=true') {
+            if (process.argv.length == 4 & process.argv[3] == 'run=true') {
                 run = true;
             }
             console.log('Converting specified store' + (run ? '' : ' as test'));
-            zipCurrDir(dir, name, vol, run);
+            zipCurrDir(dir, vol, run);
             break;
         case 'convertWholeDir':
             if (process.argv.length < 4) {
@@ -90,7 +89,7 @@ const processParams = () => {
                     run = true;
                 }
                 console.log('Converting whole store' + (run ? '' : ' as test'));
-                dirCrawler(baseDir, '', 0, run);
+                dirCrawler(baseDir, 0, run);
             }
             break;
         default:
